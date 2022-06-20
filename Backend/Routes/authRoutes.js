0a9cf18e3caa32ec6e82cff9e708997e12bc3b81
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-20 22:54:31 +0300

    Removed catch async from non async function which was triggering headers error (statuscode was being set after a response was sent)

--- Backend/Routes/authRoutes.js
+++ Backend/Routes/authRoutes.js
@@ -4,9 +4,9 @@ const router = express.Router()
 const auth = require('../Controllers/authController')
 const catchAsync = require('../Utils/catchAsync')
 
-router.get('/me', catchAsync(auth.me))
+router.get('/me', auth.me)
 router.post('/register', catchAsync(auth.register))
-router.post('/login', passport.authenticate('local'), catchAsync(auth.login))
-router.post('/logout', catchAsync(auth.logout))
+router.post('/login', passport.authenticate('local'), auth.login)
+router.post('/logout', auth.logout)
 
 module.exports = router
