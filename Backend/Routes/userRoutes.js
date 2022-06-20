c0ff88d4f8052ab0358338b744b23d4519813e0d
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-20 12:53:09 +0300

    Added a patch and get routes to the user routes

--- Backend/Routes/userRoutes.js
+++ Backend/Routes/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
+router.get('/:id',)
 router.post('/block',)
 router.post('/report',)
+router.patch('/',)
 // chat?
 
 module.exports = router
