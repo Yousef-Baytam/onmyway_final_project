cb78a37cd690744d109486f0828eee0337f1dbb9
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-20 20:05:14 +0300

    Defined an error handling class

--- Backend/Controllers/userController.js
+++ Backend/Controllers/userController.js
@@ -8,7 +8,6 @@ module.exports.getUser = async (req, res, next) => {
 }
 
 module.exports.updateUser = async (req, res, next) => {
-    // res.send(req.body)
     const user = req.user
     const update = await User.findByIdAndUpdate(user.id, { 'email': req.body.email, 'phone': req.body.phone, 'gender': req.body.gender }, { new: true })
     res.send(update)
