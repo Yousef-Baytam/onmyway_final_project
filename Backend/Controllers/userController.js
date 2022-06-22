dd4aa70f6e6785721efa0b6ff89334b37e4c3f64
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-22 22:28:41 +0300

    Added a get join requests function and changed the user controller responses syntax

--- Backend/Controllers/userController.js
+++ Backend/Controllers/userController.js
@@ -1,27 +1,33 @@
 const User = require("../Models/user")
 const Report = require('../Models/reports')
+const Post = require('../Models/posts')
 
 
 module.exports.getUser = async (req, res, next) => {
     const { id } = req.params
     const user = await User.findById(id)
-    res.send(user)
+    return res.send({ "success": true, "results": user })
+}
+
+module.exports.getJoinRequests = async (req, res, next) => {
+    const post = await Post.findById(req.params.id).populate('joinRequests.joined')
+    return res.send({ "success": true, "results": post })
 }
 
 module.exports.updateUser = async (req, res, next) => {
     const user = req.user
     const update = await User.findByIdAndUpdate(user.id, { 'email': req.body.email, 'phone': req.body.phone, 'gender': req.body.gender }, { new: true, runValidators: true })
-    res.send(update)
+    return res.send({ "success": true, "results": update })
 }
 
 module.exports.blockUser = async (req, res, next) => {
     const user = req.user
     const update = await User.findByIdAndUpdate(user.id, { $push: { blocked: req.params.id } }, { new: true })
-    res.send(update)
+    return res.send({ "success": true, "results": update })
 }
 
 module.exports.reportUser = async (req, res, next) => {
     const rep = new Report({ 'sender': req.user.id, 'reported': req.params.id, 'reportType': req.body.reportType, 'report': req.body.report })
     await rep.save()
-    res.send({ "success": true, "report": rep })
-}  
+    return res.send({ "success": true, "results": rep })
+}
--- Backend/Routes/userRoutes.js
+++ Backend/Routes/userRoutes.js
@@ -4,8 +4,11 @@ const router = express.Router()
 const catchAsync = require('../Utils/catchAsync')
 
 router.get('/:id', catchAsync(userController.getUser))
+router.get('/join_requests/:id', catchAsync())
 router.post('/block/:id', catchAsync(userController.blockUser))
 router.post('/report/:id', catchAsync(userController.reportUser))
+router.post('/:post_id/approve/:user_id', catchAsync())
+router.post('/:post_id/decline/:user_id', catchAsync())
 router.patch('/', catchAsync(userController.updateUser))
 // chat?
 
