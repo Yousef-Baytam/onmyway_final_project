4fffa16d4f3b94cefcbc4864dfcfe750123d6c0c
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-06-28 21:25:48 +0300

    Added a jwt token issuer function

---
+++ Backend/Utils/jwtGen.js
@@ -0,0 +1,23 @@
+const process = require('dotenv').config()
+const jwt = require('jsonwebtoken');
+jwt_key = process.parsed.JWT_TOKEN_SECRET
+
+const issueJWT = (user) => {
+    const expIn = '1d'
+
+    const payload = {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+        iat: Date.now()
+    }
+
+    const signedToken = jwt.sign(payload, jwt_key, { expiresIn: expIn })
+
+    return {
+        token: signedToken,
+        expires: expIn
+    }
+}
+
+module.exports = issueJWT
