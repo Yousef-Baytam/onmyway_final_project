76ff8254861f3e6e68c2e504b373ec5ee86e79ee
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-06 23:56:27 +0300

    Front-End: Added basic styling to the input component

--- Onmyway_frontend/src/components/Input.js
+++ Onmyway_frontend/src/components/Input.js
@@ -9,5 +9,16 @@ export default function Input({ placeholder, value, setValue, keyboard }) {
 }
 
 const styles = StyleSheet.create({
-
+    inputContainer: {
+        width: '100%',
+        alignItems: 'center'
+    },
+    input: {
+        width: '80%',
+        borderBottomWidth: 0.2,
+        borderColor: 'rgba(0,0,0, 0.7)',
+        paddingBottom: 10,
+        marginBottom: 25,
+        paddingLeft: 10
+    }
 });
