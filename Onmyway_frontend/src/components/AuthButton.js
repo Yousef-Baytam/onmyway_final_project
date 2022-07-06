c6f0bc075ca62c0d2fd39f085cdf5710529e6fc7
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-07 00:46:07 +0300

    Front-End: Addded authButton component

---
+++ Onmyway_frontend/src/components/AuthButton.js
@@ -0,0 +1,12 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function AuthButton() {
+    return (
+        <View>
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+});
