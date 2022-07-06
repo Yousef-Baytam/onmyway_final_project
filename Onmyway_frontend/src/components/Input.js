5a0826c2ccafad426c82c360a3bee9f9e7f8a686
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-06 23:56:06 +0300

    Front-End: Added login input state and passed props to the input component

--- Onmyway_frontend/src/components/Input.js
+++ Onmyway_frontend/src/components/Input.js
@@ -1,9 +1,9 @@
 import { StyleSheet, Text, View, TextInput } from 'react-native';
 
-export default function Input() {
+export default function Input({ placeholder, value, setValue, keyboard }) {
     return (
-        <View>
-
+        <View style={styles.inputContainer}>
+            <TextInput placeholder={placeholder} style={styles.input} value={value} onChangeText={setValue} keyboardType={keyboard} />
         </View>
     );
 }
--- Onmyway_frontend/src/screens/LoginScreen/index.js
+++ Onmyway_frontend/src/screens/LoginScreen/index.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Input from '../../components/Input';
 
 export default function Login() {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
     return (
         <View style={styles.container}>
-            <Input />
+            <Input placeholder={'Username'} value={username} setValue={setUsername} keyboard={'default'} />
+            <Input placeholder={'Password'} value={password} setValue={setPassword} keyboard={'default'} />
         </View>
     );
 }
