be6b7ac911eff958c217693ef009bd4eae10bb24
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-06 23:16:03 +0300

    Front-End: Added an input component

---
+++ Onmyway_frontend/src/components/Input.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+export default function Input() {
+    return (
+        <View>
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+
+});
--- Onmyway_frontend/src/screens/LoginScreen/index.js
+++ Onmyway_frontend/src/screens/LoginScreen/index.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Input from '../../components/Input';
 
 export default function Login() {
     return (
         <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
-            <StatusBar style="auto" />
+            <Input />
         </View>
     );
 }
