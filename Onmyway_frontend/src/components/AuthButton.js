981df529b747f54e6fbcfb1355164c3842b8f70e
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-07 00:56:52 +0300

    Front-End: Added a button style

--- Onmyway_frontend/src/components/AuthButton.js
+++ Onmyway_frontend/src/components/AuthButton.js
@@ -5,7 +5,7 @@ export default function AuthButton({ value }) {
     return (
         <View style={styles.buttonContainer}>
             <Pressable>
-                <View>
+                <View style={styles.button}>
                     <Text>{value}</Text>
                 </View>
             </Pressable>
@@ -15,6 +15,13 @@ export default function AuthButton({ value }) {
 
 const styles = StyleSheet.create({
     buttonContainer: {
-        margin: 25
+        margin: 25,
+    },
+    button: {
+        backgroundColor: '#A1CCE4',
+        paddingVertical: 20,
+        paddingHorizontal: '40%',
+        alignItems: 'center',
+        borderRadius: 10
     }
 });
