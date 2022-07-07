8c44457f7df68ccf31de69f37a6e45ada4d697c7
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-07 22:30:02 +0300

    Front-End: Added phone input package

--- Onmyway_frontend/src/screens/RegisterScreen/index.js
+++ Onmyway_frontend/src/screens/RegisterScreen/index.js
@@ -1,16 +1,20 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import EmailIcon from '../../assets/icons/EmailIcon';
 import PasswordIcon from '../../assets/icons/PasswordIcon';
 import UsernameIcon from '../../assets/icons/UsernameIcon';
 import AuthButton from '../../components/AuthButton';
 import Input from '../../components/Input';
+import PhoneInput from 'react-native-phone-input'
 
 export default function Register() {
     const [username, setUsername] = useState('')
     const [email, setEmail] = useState('')
+    const [phone, setPhone] = useState('')
     const [password, setPassword] = useState('')
     const [rePassword, setRePassword] = useState('')
+
+    const phoneInput = useRef()
 
     return (
         <View style={styles.container}>
@@ -22,6 +26,17 @@ export default function Register() {
             <View style={styles.inputContainer}>
                 <EmailIcon />
                 <Input placeholder={'Email'} value={email} setValue={setEmail} />
+            </View>
+            <View style={styles.inputContainer}>
+                <EmailIcon />
+                <PhoneInput
+                    ref={phoneInput}
+                    defaultValue={phone}
+                    defaultCode="IN"
+                    onChangeFormattedText={(e) => {
+                        setPhone(e);
+                    }}
+                />
             </View>
             <View style={styles.inputContainer}>
                 <PasswordIcon />
