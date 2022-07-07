0323e6a49441604e7ac6d698ced9e38a0214bad5
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-07 20:40:18 +0300

    Front-End: Added a signup title and a button

--- Onmyway_frontend/src/screens/RegisterScreen/index.js
+++ Onmyway_frontend/src/screens/RegisterScreen/index.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import PasswordIcon from '../../assets/icons/PasswordIcon';
+import UsernameIcon from '../../assets/icons/UsernameIcon';
+import AuthButton from '../../components/AuthButton';
+import Input from '../../components/Input';
 
 export default function Register() {
     return (
         <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
-            <StatusBar style="auto" />
+            <Text style={styles.title}>Sign up</Text>
+            <AuthButton value={"Sign up"} />
         </View>
     );
 }
