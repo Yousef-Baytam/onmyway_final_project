3fbaa5d388212ea1e727829bf06dc1af01c63ca4
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-07 20:41:56 +0300

    Front-End: Added username and password from the login page

--- Onmyway_frontend/src/screens/RegisterScreen/index.js
+++ Onmyway_frontend/src/screens/RegisterScreen/index.js
@@ -1,4 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import PasswordIcon from '../../assets/icons/PasswordIcon';
 import UsernameIcon from '../../assets/icons/UsernameIcon';
@@ -6,9 +6,20 @@ import AuthButton from '../../components/AuthButton';
 import Input from '../../components/Input';
 
 export default function Register() {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Sign up</Text>
+            <View style={styles.inputContainer}>
+                <UsernameIcon />
+                <Input placeholder={'Username'} value={username} setValue={setUsername} />
+            </View>
+            <View style={styles.inputContainer}>
+                <PasswordIcon />
+                <Input placeholder={'Password'} value={password} setValue={setPassword} password={true} />
+            </View>
             <AuthButton value={"Sign up"} />
         </View>
     );
