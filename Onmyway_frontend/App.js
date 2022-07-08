c0f8b7c13a49e54b461d40121d8cf8065b6bc3f4
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-08 16:36:32 +0300

    Front-End: Removed unused import and variable in App.js

--- Onmyway_frontend/App.js
+++ Onmyway_frontend/App.js
@@ -1,11 +1,9 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from '@react-navigation/stack';
 import 'react-native-gesture-handler'
 import UserProvider from "./src/context/UserContext";
 import StackController from "./src/navigation/StackController";
 
 export default function App() {
-  const Stack = createStackNavigator();
   return (
     <NavigationContainer>
       <UserProvider>
