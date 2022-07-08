e165817d55bb588d483a5c2cdcb759202fe22a7f
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-08 16:30:16 +0300

    Front-End: Fixed the stackController approach

--- Onmyway_frontend/App.js
+++ Onmyway_frontend/App.js
@@ -1,20 +1,15 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from '@react-navigation/stack';
-import Login from './src/screens/LoginScreen';
-import Register from './src/screens/RegisterScreen';
 import 'react-native-gesture-handler'
 import UserProvider from "./src/context/UserContext";
+import StackController from "./src/navigation/StackController";
 
 export default function App() {
   const Stack = createStackNavigator();
   return (
-
     <NavigationContainer>
       <UserProvider>
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="Register" component={Register} />
-        </Stack.Navigator>
+        <StackController />
       </UserProvider>
     </NavigationContainer>
   );
--- Onmyway_frontend/src/navigation/AuthStack.js
+++ Onmyway_frontend/src/navigation/AuthStack.js
@@ -1,19 +1,14 @@
-// import { NavigationContainer } from "@react-navigation/native";
-// import { createStackNavigator } from '@react-navigation/stack';
-// import Login from './src/screens/LoginScreen';
-// import Register from './src/screens/RegisterScreen';
-// import * as React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from '../screens/LoginScreen';
+import Register from '../screens/RegisterScreen';
 
+export function AuthStack() {
+    const Stack = createStackNavigator();
 
-// export function AuthStack() {
-//     const Stack = createStackNavigator();
-
-//     return (
-//         <NavigationContainer>
-//             <Stack.Navigator>
-//                 <Stack.Screen name="Login" component={Login} />
-//                 <Stack.Screen name="Register" component={Register} />
-//             </Stack.Navigator>
-//         </NavigationContainer>
-//     );
-// }
+    return (
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="Register" component={Register} />
+        </Stack.Navigator>
+    );
+}
