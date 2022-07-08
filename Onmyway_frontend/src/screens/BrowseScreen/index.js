b598f7fe61bbe7da68dfea295ca000015bd6d74c
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-08 16:39:24 +0300

    Front-End: Added a browseScreen

---
+++ Onmyway_frontend/src/screens/BrowseScreen/index.js
@@ -0,0 +1,19 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function Browse({ navigation }) {
+
+    return (
+        <View style={styles.container}>
+            <Text>POSTS!!!! YAAYYY</Text>
+        </View >
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
