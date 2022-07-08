40aef0f9b9f0bd51cd076b00e7a0febfee23cbbb
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-08 22:19:27 +0300

    Front-End: Added a loading context for the app

---
+++ Onmyway_frontend/src/context/LoadingContext.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+const LoadingContext = React.createContext()
+
+const LoadingProvider = ({ children }) => {
+    const [loading, setLoading] = useState(true)
+
+    const handleLoading = (e) => {
+        setLoading(e)
+    }
+
+    return (
+        <LoadingContext.Provider
+            value={{
+                loading, setLoading
+            }}
+        >
+            {children}
+        </LoadingContext.Provider>
+    )
+};
+
+export default LoadingProvider;
+
+export const useLoading = () => {
+    const { loading, handleLoading } = React.useContext(LoadingContext)
+
+    return {
+        loading, handleLoading
+    }
+}
