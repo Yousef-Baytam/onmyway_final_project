fae6ee690eac6de9aa5a6185c211c3c0aef0b184
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-08 22:20:49 +0300

    Front-End: Added Loading provider to the app

--- Onmyway_frontend/App.js
+++ Onmyway_frontend/App.js
@@ -1,14 +1,17 @@
 import { NavigationContainer } from "@react-navigation/native";
 import 'react-native-gesture-handler'
 import UserProvider from "./src/context/UserContext";
+import LaodingProvider from "./src/context/LoadingContext";
 import StackController from "./src/navigation/StackController";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <UserProvider>
-        <StackController />
-      </UserProvider>
+      <LaodingProvider>
+        <UserProvider>
+          <StackController />
+        </UserProvider>
+      </LaodingProvider>
     </NavigationContainer>
   );
 }
