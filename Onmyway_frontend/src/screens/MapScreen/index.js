570b8739e331e7615871bb2f8753fd699e9f00d6
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-10 16:26:22 +0300

    Front-End: Added a Map screen

---
+++ Onmyway_frontend/src/screens/MapScreen/index.js
@@ -0,0 +1,19 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function Map({ navigation }) {
+
+    return (
+        <View style={styles.container}>
+            <Text>Map YAAAYY!!!</Text>
+        </View >
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
