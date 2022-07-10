65210d6c5d9aeae241dd6c546176407e6b53ad19
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-10 21:52:27 +0300

    Front-End: Adjusted DrawerIcon viewbox and added the icon to the header

--- Onmyway_frontend/src/navigation/MainStack.js
+++ Onmyway_frontend/src/navigation/MainStack.js
@@ -3,12 +3,15 @@ import Browse from '../screens/BrowseScreen'
 import UserProfile from '../screens/UserProfileScreen'
 import NewPost from '../screens/NewPostScreen'
 import Map from '../screens/MapScreen'
+import DrawerToggler from '../components/DrawerToggler';
 
 export function MainStack() {
     const Stack = createStackNavigator();
 
     return (
-        <Stack.Navigator>
+        <Stack.Navigator screenOptions={{
+            headerRight: () => (<DrawerToggler />),
+        }}>
             <Stack.Screen name="Browse" component={Browse} options={{
                 headerTitle: ''
             }} />
