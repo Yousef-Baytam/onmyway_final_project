1360b8d231d58fa7d5f532a88499a4df7ca05934
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-10 17:05:54 +0300

    Front-End: Added a UserProfileHeaderButton

---
+++ Onmyway_frontend/src/components/UserProfileHeaderButton.js
@@ -0,0 +1,13 @@
+import { StyleSheet, View, Text } from 'react-native';
+import Pressable from 'react-native/Libraries/Components/Pressable/Pressable';
+
+export default function UserProfileHeaderButton() {
+    return (
+        <View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+
+});
