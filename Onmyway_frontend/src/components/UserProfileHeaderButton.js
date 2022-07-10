2312d489725647aabb18650cf1b6af36082b3afd
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-10 17:14:10 +0300

    Front-End: Added the svg to the userProfileHeaderButton component dispashed an onPress event

--- Onmyway_frontend/src/components/UserProfileHeaderButton.js
+++ Onmyway_frontend/src/components/UserProfileHeaderButton.js
@@ -1,13 +1,20 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import Pressable from 'react-native/Libraries/Components/Pressable/Pressable';
+import UserHeaderIcon from '../assets/icons/UserHeaderIcon';
 
-export default function UserProfileHeaderButton() {
+export default function UserProfileHeaderButton({ action }) {
     return (
-        <View>
+        <View style={styles.container}>
+            <Pressable onPress={action}>
+                <UserHeaderIcon />
+            </Pressable>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-
+    container: {
+        width: 50,
+        height: 50
+    }
 });
