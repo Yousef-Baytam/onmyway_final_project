363d7aa9810bbb862d950ba02a12125bc46c1d53
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-11 23:01:49 +0300

    Front-End: Added a day picker

---
+++ Onmyway_frontend/src/components/DayPicker.js
@@ -0,0 +1,25 @@
+import { StyleSheet, View } from 'react-native';
+import Pressable from 'react-native/Libraries/Components/Pressable/Pressable';
+
+export default function DayPicker({ }) {
+    return (
+        <View style={styles.container}>
+            <Pressable>
+                <View>
+                    <Text>M</Text>
+                </View>
+            </Pressable>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: '60%',
+        height: 50,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+});
