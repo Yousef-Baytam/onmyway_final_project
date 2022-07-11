022771f2085fea8ab3e072c20fed43aaf96fe22e
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-11 23:04:48 +0300

    Front-End: Added the day picker to the DateInput

--- Onmyway_frontend/src/components/DateInput.js
+++ Onmyway_frontend/src/components/DateInput.js
@@ -2,13 +2,16 @@ import { StyleSheet, View } from 'react-native';
 import RepeatIcon from '../assets/icons/RepeatIcon';
 import Checkbox from 'expo-checkbox';
 import DatePicker from './DatePicker';
+import DayPicker from './DayPicker';
 
 export default function DateInput({ repeat, setRepeat, date, setDate }) {
     return (
         <View style={styles.container}>
             <RepeatIcon />
             <Checkbox value={repeat} onValueChange={setRepeat} color={'#A1CCE4'} style={{ marginBottom: 20, marginHorizontal: 15 }} />
-            <DatePicker date={date} setDate={setDate} placeholder={'Date'} AuthInput={false} />
+            {repeat ?
+                <DayPicker /> :
+                <DatePicker date={date} setDate={setDate} placeholder={'Date'} AuthInput={false} />}
         </View>
     );
 }
--- Onmyway_frontend/src/components/DayPicker.js
+++ Onmyway_frontend/src/components/DayPicker.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import Pressable from 'react-native/Libraries/Components/Pressable/Pressable';
 
 export default function DayPicker({ }) {
