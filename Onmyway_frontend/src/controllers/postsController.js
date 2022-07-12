8352c4d330475b10aa0afcf87df62445ad333ed7
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-13 00:38:07 +0300

    Front-End: Added the token to the addPost post request

--- Onmyway_frontend/src/controllers/postsController.js
+++ Onmyway_frontend/src/controllers/postsController.js
@@ -1,12 +1,17 @@
 import axios from 'axios'
 import { url } from '../constants/vars'
+import storage from "../storage/asyncStorage";
 
 const addPost = async (data) => {
     try {
+        const token = await storage.load({ key: 'token' })
         let res = await axios({
             url: `${ url }/post/new`,
             method: "POST",
-            data: data
+            data: data,
+            headers: {
+                Authorization: `bearer ${ token }`
+            }
         })
         return res.data
     }
