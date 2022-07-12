784492d26928a7e9c5cfab5f015bf4cb49de9169
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-12 15:33:47 +0300

    Front-End: Added a ShareExpenses component

---
+++ Onmyway_frontend/src/components/ShareExpenses.js
@@ -0,0 +1,37 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function ShareExpenses({ text, value, setValue }) {
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.view}>
+                <Text style={styles.text}>{text}</Text>
+            </View>
+            <View style={styles.view}>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: '80%',
+        height: 100,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
+    },
+    view: {
+        height: 80,
+        width: '50%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderBottomWidth: 0.2,
+        paddingVertical: 25,
+        borderColor: 'rgba(0,0,0, 0.7)',
+    },
+    text: {
+        paddingBottom: 5,
+    }
+});
