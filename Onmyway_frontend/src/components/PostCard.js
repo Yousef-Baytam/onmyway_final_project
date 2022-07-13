9338994fdc698f2c3f7852c4445bd325074b7081
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-13 14:15:50 +0300

    Front-End: Added the image to the card and added basic styling

--- Onmyway_frontend/src/components/PostCard.js
+++ Onmyway_frontend/src/components/PostCard.js
@@ -3,9 +3,9 @@ import { StyleSheet, View, Text, Image } from 'react-native';
 export default function PostCard(data) {
     return (
         <View style={styles.container}>
-            <View>
+            <View style={styles.cardContainer}>
                 <View>
-                    <Image />
+                    <Image source={require('../assets/blank-profile.webp')} style={styles.image} />
                 </View>
                 <View>
                     <View>
@@ -45,7 +45,20 @@ export default function PostCard(data) {
 const styles = StyleSheet.create({
     container: {
         width: '85%',
+        height: 120,
         alignItems: 'flex-start',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        marginVertical: 20
     },
+    cardContainer: {
+        width: '100%',
+        height: 120,
+        backgroundColor: '#EAEAEA',
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    image: {
+        width: 50,
+        height: 50
+    }
 });
--- Onmyway_frontend/src/screens/BrowseScreen/index.js
+++ Onmyway_frontend/src/screens/BrowseScreen/index.js
@@ -1,10 +1,12 @@
 import { StyleSheet, View } from 'react-native';
 import AddPost from '../../components/AddPost';
+import PostCard from '../../components/PostCard';
 
 export default function Browse({ navigation }) {
 
     return (
         <View style={styles.container}>
+            <PostCard />
             <AddPost action={() => navigation.navigate('NewPost')} />
         </View >
     );
