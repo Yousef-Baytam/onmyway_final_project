5219850571cec386e6a4846b0a6a2943389f773f
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-15 00:54:09 +0300

    Front-End: Added a function to pick image from the phone library

--- Onmyway_frontend/src/components/UserImage.js
+++ Onmyway_frontend/src/components/UserImage.js
@@ -2,6 +2,20 @@ import { StyleSheet, Image } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 
 export default function UserImage({ image, setImage }) {
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 4],
+            quality: 1,
+        })
+        console.log(result)
+
+        if (!result.cancelled) {
+            setImage(result.uri)
+        }
+    }
+
     return (
         <>
             <Image style={styles.image} source={require('../assets/blank-profile.webp')} />
