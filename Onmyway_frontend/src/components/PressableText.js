4e8dde8f667bcb816a84b7ecf42d4139b483d510
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-14 23:27:50 +0300

    Front-End: Added a pressable text component

---
+++ Onmyway_frontend/src/components/PressableText.js
@@ -0,0 +1,18 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function PressableText({ Text, action }) {
+    return (
+        <View style={styles.container}>
+
+        </View >
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
