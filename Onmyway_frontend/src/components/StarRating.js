d4e9157666f3d880c9268f98ff60bc02e4b5f9a6
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-14 21:57:57 +0300

    Front-End: Adjusted the star rating component to display average user rating, renders readonly on display true, and changed its style

--- Onmyway_frontend/src/components/StarRating.js
+++ Onmyway_frontend/src/components/StarRating.js
@@ -1,6 +1,5 @@
 import { Rating } from 'react-native-ratings';
-import { StyleSheet, View, Text, Image } from 'react-native';
-import { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
 
 export default function StarRating({ rating, display }) {
     const onFinishRating = (e) => {
@@ -9,24 +8,43 @@ export default function StarRating({ rating, display }) {
 
     return (
         <View style={styles.container}>
-            <Rating
-                fractions={2}
-                jumpValue={0.5}
+            {display ? <Rating
+                type='custom'
+                startingValue={rating}
                 ratingColor='#A1CCE4'
-                onFinishRating={onFinishRating}
-                style={{ paddingVertical: 10 }}
+                tintColor='#fff'
+                ratingBackgroundColor='#EAEAEA'
+                readonly={true}
+                imageSize={25}
+                style={styles.stars}
             />
+                : <Rating
+                    fractions={2}
+                    jumpValue={0.5}
+                    type='custom'
+                    startingValue={rating}
+                    ratingColor='#A1CCE4'
+                    tintColor='#fff'
+                    ratingBackgroundColor='#EAEAEA'
+                    imageSize={25}
+                    onFinishRating={onFinishRating}
+                    style={styles.stars}
+                />
+            }
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        width: '85%',
+        width: '100%',
         alignItems: 'flex-start',
         flexDirection: "row",
         justifyContent: 'center'
     },
+    stars: {
+        paddingVertical: 10
+    }
 });
 
 StarRating.defaultProps = {
