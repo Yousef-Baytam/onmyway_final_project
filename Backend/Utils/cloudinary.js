3dbb0955f6019e975f207f92680925fc636c50ae
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-15 00:31:48 +0300

    Back-End: Added cloudinary file, config, and uploadImage function

---
+++ Backend/Utils/cloudinary.js
@@ -0,0 +1,24 @@
+const cloudinary = require('cloudinary').v2
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET
+})
+
+module.exports.uploadImage = async (imagePath) => {
+
+    const options = {
+        use_filename: true,
+        unique_filename: false,
+        overwrite: true,
+    };
+
+    try {
+        const result = await cloudinary.uploader.upload(imagePath, options);
+        console.log(result);
+        return result.public_id;
+    } catch (error) {
+        console.error(error);
+    }
+};
