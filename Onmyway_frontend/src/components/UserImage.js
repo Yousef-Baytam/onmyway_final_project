66858ca64dfe90cdd882db2960c2d7a5cf355c36
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-15 00:48:10 +0300

    Front-End: Added a UserImage component

---
+++ Onmyway_frontend/src/components/UserImage.js
@@ -0,0 +1,16 @@
+import { StyleSheet, View } from 'react-native';
+
+export default function UserImage({ user }) {
+    return (
+        <View style={styles.container}>
+
+        </View >
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        height: '100%',
+    },
+});
