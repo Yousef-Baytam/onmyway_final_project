286e9575a02c041216537838bbd0470148f6ffb0
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-14 22:07:10 +0300

    Front-End: Added a user profile body component

---
+++ Onmyway_frontend/src/components/UserProfileBody.js
@@ -0,0 +1,17 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function UserProfileBody({ }) {
+    return (
+        <View style={styles.container}>
+        </View >
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        alignItems: 'flex-start',
+        flexDirection: "row",
+        justifyContent: 'center'
+    },
+});
