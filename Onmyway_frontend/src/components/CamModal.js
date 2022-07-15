66f2ae3fcd6060fb96f080fd109a3d66068f4fc0
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-15 16:01:48 +0300

    Front-End: Added camera permissions and relatd components

--- Onmyway_frontend/src/components/CamModal.js
+++ Onmyway_frontend/src/components/CamModal.js
@@ -1,7 +1,27 @@
 import { StyleSheet, View, Modal } from 'react-native';
 import Pressable from 'react-native/Libraries/Components/Pressable/Pressable';
+import { Camera, CameraType } from 'expo-camera';
+import { useEffect, useState } from 'react';
 
 export default function CamModal({ modalVisible, setModalVisible }) {
+    const [hasPermission, setHasPermission] = useState(null);
+    const [type, setType] = useState(CameraType.back);
+
+    useEffect(() => {
+        (async () => {
+            const { status } = await Camera.requestCameraPermissionsAsync();
+            setHasPermission(status === 'granted');
+        })();
+    }, []);
+
+    if (hasPermission === null) {
+        return <View />;
+    }
+
+    if (hasPermission === false) {
+        return <Text>No access to camera</Text>;
+    }
+
     return (
         <View>
             <Modal
@@ -9,8 +29,7 @@ export default function CamModal({ modalVisible, setModalVisible }) {
                 transparent={false}
                 visible={modalVisible}
                 onRequestClose={() => {
-                    Alert.alert("Modal has been closed.")
-                    setModalVisible(!modalVisible);
+                    setModalVisible(!modalVisible)
                 }}
             >
             </Modal>
