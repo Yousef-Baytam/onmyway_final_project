9c512bf3faf30cb50d4c71fbcee195e4b62a32bd
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-15 19:25:42 +0300

    Front-End: Adjusted the rating round to the nearest 0.5

--- Onmyway_frontend/src/screens/UserProfileScreen/Index.js
+++ Onmyway_frontend/src/screens/UserProfileScreen/Index.js
@@ -11,7 +11,7 @@ import { useUser } from '../../context/UserContext';
 export default function UserProfile({ navigation }) {
     const { user, handleUser } = useUser()
     const [image, setImage] = useState(user.image.url || null);
-    const [rating, setRating] = useState(Math.round(user.reviews.reduce((a, b) => a + b.rating, 0) / user.reviews.length))
+    const [rating, setRating] = useState(Math.round((user.reviews.reduce((a, b) => a + b.rating, 0) / user.reviews.length) * 2) / 2)
 
     return (
         <View style={styles.container}>
