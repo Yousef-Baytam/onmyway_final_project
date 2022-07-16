cdd7f30adaa7bf9bc2cf92ce4242447130acb11c
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-16 21:56:40 +0300

    Front-End: Added a AllReviewsModal

---
+++ Onmyway_frontend/src/components/AllReviewModal.js
@@ -0,0 +1,42 @@
+import { StyleSheet, View, Text, Modal } from 'react-native';
+import StarRating from './StarRating';
+
+
+export default function AllReviewModal({ user }) {
+
+    return (
+        <Modal
+            animationType="slide"
+            visible={showReviewModal}
+            onRequestClose={() => {
+                setShowReviewModal(!showReviewModal);
+            }}>
+            <View style={styles.reviewContainer}>
+                <StarRating rating={newRating} setRating={setNewRating} />
+                <Text style={styles.text}></Text>
+            </View>
+        </Modal>
+    );
+}
+
+const styles = StyleSheet.create({
+    reviewContainer: {
+        width: '100%',
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    buttonContainer: {
+        width: '100%',
+        marginVertical: 20,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    text: {
+        width: '90%',
+        height: '20%',
+        backgroundColor: '#EAEAEA',
+        borderRadius: 10,
+        padding: 20
+    },
+});
