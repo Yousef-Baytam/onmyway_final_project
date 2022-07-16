7b6966885bb3553632a2831e54303536309e044b
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-16 22:29:09 +0300

    Front-End: Adjusted the schema of the usercard

--- Onmyway_frontend/src/components/ReviewCard.js
+++ Onmyway_frontend/src/components/ReviewCard.js
@@ -1,11 +1,18 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
+import StarRating from './StarRating';
 
-export default function ReviewCard({ items, checked, setChecked, AuthInput, custom }) {
+export default function ReviewCard({ body, author, rating }) {
 
     return (
         <View style={styles.inputContainer}>
-            <StarRating />
-            <Text style={styles.text}></Text>
+            <View>
+                <View>
+                    <Image />
+                    <Text>{author.username}</Text>
+                </View>
+                <StarRating rating={item.rating} display={true} />
+                <Text style={styles.text}>{body}</Text>
+            </View>
         </View>
     );
 }
