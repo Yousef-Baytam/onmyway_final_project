e3441f938f59de7b15ad2f868b91638f4d6c1a87
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-18 00:02:16 +0300

    Back-End: Fixed the rating state to be 0 if there was no reviews

--- Onmyway_frontend/src/screens/UserProfileScreen/Index.js
+++ Onmyway_frontend/src/screens/UserProfileScreen/Index.js
@@ -14,7 +14,7 @@ export default function UserProfile({ navigation }) {
     const [visible, setVisible] = useState(false)
     const { user, handleUser } = useUser()
     const [image, setImage] = useState(user.image.url || null);
-    const [rating, setRating] = useState(Math.round((user.reviews.reduce((a, b) => a + b.rating, 0) / user.reviews.length) * 2) / 2)
+    const [rating, setRating] = useState(user.reviews.length ? Math.round((user.reviews.reduce((a, b) => a + b.rating, 0) / user.reviews.length) * 2) / 2 : 0)
     const [showAllReviews, setShowAllReviews] = useState(false)
 
     return (
