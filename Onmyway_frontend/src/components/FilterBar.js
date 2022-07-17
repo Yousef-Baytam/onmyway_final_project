10a4f89b2638e0ec327752facf207c5ca7c6042d
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-17 20:10:29 +0300

    Front-End: Added a filter bar component

---
+++ Onmyway_frontend/src/components/FilterBar.js
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+export default function Input({ }) {
+
+    return (
+        <View style={styles.container}>
+
+        </View >
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: '85%',
+        alignItems: 'flex-start',
+        flexDirection: "row",
+        justifyContent: 'center'
+    },
+});
