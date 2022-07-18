84055d35b2180725b04fd54aac35a9750766fca7
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-19 00:52:53 +0300

    Front-End: Added firebase configuration and setup file

---
+++ Onmyway_frontend/firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp, getApp } from 'firebase/app'
+import { initializeFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
+import { firebaseApiKey } from '@env'
+
+const firebaseConfig = {
+    apiKey: firebaseApiKey,
+    authDomain: "onmyway-168af.firebaseapp.com",
+    projectId: "onmyway-168af",
+    storageBucket: "onmyway-168af.appspot.com",
+    messagingSenderId: "976356158167",
+    appId: "1:976356158167:web:fc4bbbc673a58cac1f9e92",
+    measurementId: "G-C1VHM8EREY"
+}
+
+const app = initializeApp(firebaseConfig)
+
+const auth = getAuth(app)
+const db = initializeFirestore(app, { experimentalForceLongPolling: true })
+
+export { db, auth }
