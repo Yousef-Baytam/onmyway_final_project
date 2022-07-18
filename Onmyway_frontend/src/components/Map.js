a2e2da0980462eaeb3adbdf325a5db1d99c2ed2b
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-18 17:17:17 +0300

    Front-End: Added the modal and maoView elements to the map component and container style

--- Onmyway_frontend/src/components/Map.js
+++ Onmyway_frontend/src/components/Map.js
@@ -1,14 +1,27 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Modal } from 'react-native';
+import MapView from 'react-native-maps';
 
-export default function Map({ }) {
+export default function Map({ showMapModal, setShowMapModal }) {
 
     return (
-        <View style={styles.container}>
-
-        </View>
+        <Modal
+            animationType="slide"
+            visible={showMapModal}
+            onRequestClose={() => {
+                setShowMapModal(!showMapModal);
+            }}>
+            <View style={styles.container}>
+                <MapView style={styles.map} />
+            </View>
+        </Modal>
     );
 }
 
 const styles = StyleSheet.create({
-
+    container: {
+        width: '100%',
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
 });
