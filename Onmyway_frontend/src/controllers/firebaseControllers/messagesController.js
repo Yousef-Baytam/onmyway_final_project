090b6e2e470953192b048da131739d676631f9ae
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-20 00:39:33 +0300

    Front-End: Added a function to add a message as a subcollection for a chatRoom

---
+++ Onmyway_frontend/src/controllers/firebaseControllers/messagesController.js
@@ -0,0 +1,12 @@
+import { } from "firebase/firestore"
+import { db } from "../../../firebase"
+
+const addChatRoom = async (roomId, message) => {
+    try {
+        const messageRef = await db.collection('chatRooms').doc(roomId)
+            .collection('messages').doc(message)
+        console.log(messageRef)
+    } catch (e) {
+        console.log(e)
+    }
+}
