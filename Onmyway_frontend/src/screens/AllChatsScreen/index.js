ea12d087cc20248d52d114c7fcd7febbd84ae336
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-19 13:57:21 +0300

    Front-End: Added a query condition for the logged user id to be in the chat room

--- Onmyway_frontend/src/screens/AllChatsScreen/index.js
+++ Onmyway_frontend/src/screens/AllChatsScreen/index.js
@@ -1,15 +1,17 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, ActivityIndicator } from 'react-native';
 import { collection, query, where, onSnapshot } from "firebase/firestore";
 import { useEffect, useState } from 'react';
 import { db } from '../../../firebase'
-import { ActivityIndicator } from 'react-native-web';
+import { useUser } from '../../context/UserContext';
 
 export default function AllChats({ navigation }) {
+    const { user } = useUser()
+
     const [chatThreads, setChatThreads] = useState([])
     const [loading, setLoading] = useState(true)
 
     useEffect(() => {
-        const q = query(collection(db, "chatRooms"))
+        const q = query(collection(db, "chatRooms"), where('userLocalDbIds', 'array-contains', user._id))
         const unsubscribe = onSnapshot(q, (querySnapshot) => {
             const chatRooms = []
             querySnapshot.forEach((doc) => {
