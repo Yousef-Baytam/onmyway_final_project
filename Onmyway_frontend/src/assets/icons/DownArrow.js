47296e2703258202a3716fd9ab2642b11348b06b
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-19 23:32:51 +0300

    Front-End: Added a down arrow icon

---
+++ Onmyway_frontend/src/assets/icons/DownArrow.js
@@ -0,0 +1,15 @@
+import Svg, { Path } from 'react-native-svg';
+
+export default function DownArrow() {
+
+    return (
+        <Svg
+            width={32}
+            height={32}
+            viewBox="0 0 32 32"
+            fill='#A1CCE4'
+        >
+            <Path d="M227.5 0C101.855 0 0 101.855 0 227.5S101.855 455 227.5 455 455 353.145 455 227.5 353.145 0 227.5 0zm0 327.148L99.411 199.476l21.178-21.248L227.5 284.791l106.911-106.563 21.178 21.248L227.5 327.148z" />
+        </Svg>
+    );
+}
