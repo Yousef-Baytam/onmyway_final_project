5c7f3282a04356a8a0cbbe1a50d790b512825dd3
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-19 10:52:08 +0300

    Front-End: Adjusted the firebase addChatRoom function to take array of 2 emails of the 2 users

--- Onmyway_frontend/src/controllers/firebaseControllers/chatRooms.js
+++ Onmyway_frontend/src/controllers/firebaseControllers/chatRooms.js
@@ -1,8 +1,15 @@
 import { collection, addDoc } from "firebase/firestore"
 import { db } from "../../../firebase"
 
-const addChatRoom = async () => {
-    const docRef = await addDoc(collection(db, "chatRooms"), {
-
-    });
+const addChatRoom = async (email1, email2) => {
+    try {
+        const docRef = await addDoc(collection(db, "chatRooms"), {
+            users: [email1, email2]
+        })
+        console.log(docRef)
+    } catch (e) {
+        console.log(e)
+    }
 }
+
+export { addChatRoom }
