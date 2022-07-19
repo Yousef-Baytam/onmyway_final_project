385be0f9d01b7e43df2cc5b33549d89ff14522d3
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-19 13:10:56 +0300

    Front-End: Added the basic chat template from react native gifted chat

--- Onmyway_frontend/src/screens/ChatScreen/index.js
+++ Onmyway_frontend/src/screens/ChatScreen/index.js
@@ -1,15 +1,43 @@
 import { useRoute } from '@react-navigation/native';
+import { useCallback, useState } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
 
 export default function Chat({ navigation }) {
     const route = useRoute()
     const chatRoom = route.params
     console.log(chatRoom)
 
+    const [messages, setMessages] = useState([
+        {
+            _id: 0,
+            text: 'New room created.',
+            createdAt: new Date().getTime(),
+            system: true
+        },
+        {
+            _id: 1,
+            text: 'Henlo!',
+            createdAt: new Date().getTime(),
+            user: {
+                _id: 2,
+                name: 'Test User'
+            }
+        }
+    ]);
+
+    const onSend = useCallback((messages = []) => {
+        setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+    }, [])
+
     return (
-        <View style={styles.container}>
-            <Text>Chat YAAAYY!!!</Text>
-        </View >
+        <GiftedChat
+            messages={messages}
+            onSend={messages => onSend(messages)}
+            user={{
+                _id: 1,
+            }}
+        />
     );
 }
 
