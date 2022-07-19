fd7f70b70de82e4698811e4298a27245032009f9
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-19 13:52:07 +0300

    Front-End: Added a live listener for chatrooms created

--- Onmyway_frontend/src/screens/AllChatsScreen/index.js
+++ Onmyway_frontend/src/screens/AllChatsScreen/index.js
@@ -1,6 +1,27 @@
 import { StyleSheet, View, Text } from 'react-native';
+import { collection, query, where, onSnapshot } from "firebase/firestore";
+import { useEffect, useState } from 'react';
+import { db } from '../../../firebase'
 
 export default function AllChats({ navigation }) {
+    const [chatThreads, setChatThreads] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const q = query(collection(db, "chatRooms"))
+        const unsubscribe = onSnapshot(q, (querySnapshot) => {
+            const chatRooms = []
+            querySnapshot.forEach((doc) => {
+                chatRooms.push(doc.data())
+            })
+            setChatThreads(chatRooms)
+
+            if (loading) {
+                setLoading(false);
+            }
+        })
+        return () => unsubscribe()
+    }, [])
 
     return (
         <View style={styles.container}>
