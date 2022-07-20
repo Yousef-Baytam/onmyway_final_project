0cb76c3f47da61f86f579e46cbffae1181b38734
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-21 01:27:49 +0300

    Front-End: Added a useEffect in the tabNav to get the chatrooms of the user

--- Onmyway_frontend/src/navigation/TabNav.js
+++ Onmyway_frontend/src/navigation/TabNav.js
@@ -7,10 +7,51 @@ import MessagesIcon from '../assets/icons/MessagesIcon';
 import Browse from '../screens/BrowseScreen';
 import NotificationsBellIcon from '../assets/icons/NotificationsBellIcon';
 import BackArrow from '../components/BackArrow';
+import { useEffect, useState } from 'react';
+import { collection, query, where, onSnapshot, orderBy } from "firebase/firestore";
+import { db } from '../../firebase'
+import { useUser } from '../context/UserContext';
 
 const Tab = createBottomTabNavigator();
 
 export default function TabNav() {
+    const [newMessages, setNewMessages] = useState(null)
+    const { user } = useUser()
+
+    useEffect(() => {
+        const q = query(collection(db, "chatRooms"), where('userLocalDbIds', 'array-contains', user._id), orderBy('latestMessage.createdAt', "desc"))
+        const unsubscribe = onSnapshot(q, (querySnapshot) => {
+            const chatRooms = []
+            querySnapshot.forEach((doc) => {
+                chatRooms.push({ ...doc.data(), id: doc.id })
+            })
+
+            let msgs = chatRooms.map((e) => {
+                return {
+                    userTag: e.users1.userId == user._id ? 'users2' : 'users1',
+                    readStatus: e.readStatus,
+                    sender: e.sender,
+                    numberOfMessages: e.numberOfMessages
+                }
+            })
+            console.log(msgs)
+
+            // setThreadsUsers(chatRooms.map((e) => {
+            //     return {
+            //         id: e.id,
+            //         user: e.users1.userId == user._id ? e.users2 : e.users1,
+            //         lastMessage: e.latestMessage,
+            //         userTag: e.users1.userId == user._id ? 'users2' : 'users1',
+            //         readStatus: e.readStatus,
+            //         sender: e.sender,
+            //         numberOfMessages: e.numberOfMessages
+            //     }
+            // }))
+
+        })
+        return () => unsubscribe()
+    }, [])
+
     return (
         <Tab.Navigator screenOptions={{
             tabBarShowLabel: false, tabBarStyle: {
