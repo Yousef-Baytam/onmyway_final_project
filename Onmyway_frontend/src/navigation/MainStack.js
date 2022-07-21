0ee622935e3caf77c084fb63ae5c91475a51df1c
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-22 01:38:05 +0300

    Front-End: Added the push notifications template from expo docs

--- Onmyway_frontend/src/navigation/MainStack.js
+++ Onmyway_frontend/src/navigation/MainStack.js
@@ -9,38 +9,44 @@ import UserProfileHeaderButton from '../components/UserProfileHeaderButton';
 import BackArrow from '../components/BackArrow';
 import Post from '../screens/PostScreen';
 import Profile from '../screens/ProfileScreen';
+import Notification from '../../NotificationRegister';
+import { useState } from 'react';
 
 export function MainStack() {
     const Stack = createStackNavigator();
+    const [expoPushToken, setExpoPushToken] = useState('');
 
     return (
-        <Stack.Navigator screenOptions={({ navigation }) => ({
-            cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
-            headerRight: () => (
-                <DrawerToggler action={() => navigation.dispatch(DrawerActions.openDrawer())} />), headerMode: 'float', headerStyle: { elevation: 0 }
-        })
-        }>
-            <Stack.Screen name="Browse" component={Browse} options={({ navigation }) => ({
-                headerTitle: 'Browse', headerTitleAlign: 'center', headerLeft: () => (
-                    <UserProfileHeaderButton action={() => { navigation.navigate('UserProfile') }} />),
-
+        <>
+            <Stack.Navigator screenOptions={({ navigation }) => ({
+                cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+                headerRight: () => (
+                    <DrawerToggler action={() => navigation.dispatch(DrawerActions.openDrawer())} />), headerMode: 'float', headerStyle: { elevation: 0 }
             })
-            } />
-            <Stack.Group screenOptions={({ navigation }) => ({
-                headerLeft: () => (
-                    <BackArrow action={() => { navigation.dispatch(CommonActions.goBack()) }} />), headerTitleAlign: 'center'
-            })}>
-                <Stack.Screen name="UserProfile" component={UserProfile} options={{
-                    headerTitle: 'My Profile'
-                }} />
-                <Stack.Screen name="NewPost" component={NewPost} options={{
-                    headerTitle: 'New Post'
-                }} />
-                <Stack.Screen name="Post" component={Post} />
-                <Stack.Screen name="Profile" component={Profile} options={({ route }) => ({ title: route.params.username })} />
-                <Stack.Screen name="Map" component={Map} />
-            </Stack.Group>
+            }>
+                <Stack.Screen name="Browse" component={Browse} options={({ navigation }) => ({
+                    headerTitle: 'Browse', headerTitleAlign: 'center', headerLeft: () => (
+                        <UserProfileHeaderButton action={() => { navigation.navigate('UserProfile') }} />),
 
-        </Stack.Navigator>
+                })
+                } />
+                <Stack.Group screenOptions={({ navigation }) => ({
+                    headerLeft: () => (
+                        <BackArrow action={() => { navigation.dispatch(CommonActions.goBack()) }} />), headerTitleAlign: 'center'
+                })}>
+                    <Stack.Screen name="UserProfile" component={UserProfile} options={{
+                        headerTitle: 'My Profile'
+                    }} />
+                    <Stack.Screen name="NewPost" component={NewPost} options={{
+                        headerTitle: 'New Post'
+                    }} />
+                    <Stack.Screen name="Post" component={Post} />
+                    <Stack.Screen name="Profile" component={Profile} options={({ route }) => ({ title: route.params.username })} />
+                    <Stack.Screen name="Map" component={Map} />
+                </Stack.Group>
+
+            </Stack.Navigator>
+            <Notification etExpoPushToken={setExpoPushToken} expoPushToken={expoPushToken} />
+        </>
     );
 }
