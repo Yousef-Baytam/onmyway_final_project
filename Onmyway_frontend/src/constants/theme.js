acd39193dbb67a737a612f487e77a0d352c3fd7b
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-23 00:46:02 +0300

    Front-End: Added the basic theme colors

--- Onmyway_frontend/src/constants/theme.js
+++ Onmyway_frontend/src/constants/theme.js
@@ -1,4 +1,15 @@
 export const THEME_OPTIONS = {
-    LIGHT: 'light',
-    DARK: 'dark'
+    LIGHT: {
+        bg: '#fff',
+        outline: '#858585',
+        light: '#EAEAEA',
+        text: '#000'
+    },
+    DARK: {
+        bg: '#000',
+        outline: '#ACACAC',
+        light: '#EAEAEA',
+        text: '#000'
+    }
 }
+
