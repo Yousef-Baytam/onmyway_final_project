d71af4b684ffd04ce6020e21a5f25d5995219541
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-23 21:53:53 +0300

    Front-End-Admin: Added a useEffect and a handle get users function and a users state

--- onmyway_admin/src/components/AllUsers.js
+++ onmyway_admin/src/components/AllUsers.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from './Header'
 
 export default function AllUsers() {
+    const [users, setUsers] = useState(null)
+
+    const handleGetUsers = async () => {
+        const res = await getUsers()
+        console.log(res)
+    }
+
+    useEffect(() => {
+        handleGetUsers()
+    }, [])
+
     return (
         <div>
             <Header title={'All Users'} />
+
         </div>
     )
 }
