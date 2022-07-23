8764f8e7f52fed9c731f049bb49d40f513e33c27
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-23 21:42:08 +0300

    Front-End-Admin: Added the basic header elements

--- onmyway_admin/src/components/Header.js
+++ onmyway_admin/src/components/Header.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Input from './Input'
 
-export default function Header() {
+export default function Header({ title }) {
+    const [search, setSearch] = useState('')
     return (
-        <div>Header</div>
+        <div>
+            <div>
+                {title}
+            </div>
+            <div>
+                <select>
+                    <option />
+                </select>
+            </div>
+            <div>
+                <Input name={search} placeholder={'Search'} type={'text'} value={search} setValue={setSearch} noLabel={true} />
+            </div>
+        </div>
     )
 }
--- onmyway_admin/src/components/Input.js
+++ onmyway_admin/src/components/Input.js
@@ -1,12 +1,16 @@
 import React from 'react'
 
-export default function Input({ name, placeholder, type, value, setValue }) {
+export default function Input({ name, placeholder, type, value, setValue, noLabel }) {
     return (
         <div className='inputComp'>
-            <label htmlFor={`${ name }`}>{placeholder}</label><br />
+            {!noLabel && <><label htmlFor={`${ name }`}>{placeholder}</label><br /></>}
             <input type={type} name={name} id={`${ name }`} placeholder={placeholder} value={value} onChange={(e) => {
                 setValue(e.target.value)
             }} />
         </div>
     )
+}
+
+Input.defaultProps = {
+    noLabel: false
 }
