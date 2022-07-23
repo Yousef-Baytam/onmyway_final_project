45b94407841f44870f7413c2a61a90abdbee7cad
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-23 15:48:59 +0300

    Front-End: Stored the theme in the asyncStorage

--- Onmyway_frontend/src/context/ThemeContext.js
+++ Onmyway_frontend/src/context/ThemeContext.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import THEME_OPTIONS from '../constants/theme';
+import storage from '../storage/asyncStorage';
 
 const ThemeContext = React.createContext()
 
@@ -7,8 +8,9 @@ const ThemeProvider = ({ children }) => {
 
     const [theme, setTheme] = useState(THEME_OPTIONS.LIGHT)
 
-    const toggleTheme = () => {
+    const toggleTheme = async () => {
         setTheme(theme == THEME_OPTIONS.LIGHT ? THEME_OPTIONS.DARK : THEME_OPTIONS.LIGHT)
+        await storage.save({ key: 'theme', data: theme == THEME_OPTIONS.LIGHT ? 'DARK' : 'LIGHT' })
     }
     return (
         <ThemeContext.Provider
