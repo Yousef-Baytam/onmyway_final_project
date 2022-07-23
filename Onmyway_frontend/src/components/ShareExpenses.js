ea3edd2cee12f56a3a339765087912741e130a38
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-23 13:10:27 +0300

    Front-End: Themed the share expenses componenr

--- Onmyway_frontend/src/components/ShareExpenses.js
+++ Onmyway_frontend/src/components/ShareExpenses.js
@@ -1,14 +1,15 @@
 import Checkbox from 'expo-checkbox';
 import { StyleSheet, View, Text } from 'react-native';
+import { useTheme } from '../context/ThemeContext';
 
 export default function ShareExpenses({ text, value, setValue, display }) {
-
+    const { theme } = useTheme()
     return (
         <View style={styles.container}>
-            <View style={styles.view}>
-                <Text style={styles.text}>{text}</Text>
+            <View style={[styles.view, { borderColor: theme.outline }]}>
+                <Text style={[styles.text, { color: theme.text }]}>{text}</Text>
             </View>
-            <View style={styles.view}>
+            <View style={[styles.view, { borderColor: theme.outline }]}>
                 {
                     display ?
                         <Checkbox value={value} disabled={true} color={'#A1CCE4'} style={{ marginHorizontal: 18, width: 15, height: 15 }} />
