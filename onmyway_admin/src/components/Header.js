657c9ba98eef680e265c114309ec9db4610dee21
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-23 21:43:13 +0300

    Front-End-Admin: Wrpped the dropdown and the input with a container

--- onmyway_admin/src/components/Header.js
+++ onmyway_admin/src/components/Header.js
@@ -4,17 +4,19 @@ import Input from './Input'
 export default function Header({ title }) {
     const [search, setSearch] = useState('')
     return (
-        <div>
+        <div className='haderContainer'>
             <div>
                 {title}
             </div>
             <div>
-                <select>
-                    <option />
-                </select>
-            </div>
-            <div>
-                <Input name={search} placeholder={'Search'} type={'text'} value={search} setValue={setSearch} noLabel={true} />
+                <div>
+                    <select>
+                        <option />
+                    </select>
+                </div>
+                <div>
+                    <Input name={search} placeholder={'Search'} type={'text'} value={search} setValue={setSearch} noLabel={true} />
+                </div>
             </div>
         </div>
     )
