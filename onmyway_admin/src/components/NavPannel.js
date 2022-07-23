26897d02d063ddb4395a1347e244f85ea99e4a2b
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-23 20:39:12 +0300

    Front-End-Admin: Styled the pannelTabs buttons

--- onmyway_admin/src/components/NavPannel.js
+++ onmyway_admin/src/components/NavPannel.js
@@ -1,17 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function NavPannel() {
+    const [focused, setFocused] = useState(null)
     return (
         <div className='pannelContainer'>
             <div>
-                <div>
+                <div className={focused == 'users' ? 'focused' : 'pannelTab'}>
                     Users
                 </div>
-                <div>
+                <div className={focused == 'reports' ? 'focused' : 'pannelTab'}>
                     Reports
                 </div>
             </div>
-            <div>
+            <div className='pannelTab logout'>
                 Logout
             </div>
         </div>
