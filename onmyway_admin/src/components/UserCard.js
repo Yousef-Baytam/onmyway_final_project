f81e53cc31e020a9cf78f73c7fc3210f958eed3c
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-23 23:15:19 +0300

    Front-End-Admin: Added the user info to each card

--- onmyway_admin/src/components/UserCard.js
+++ onmyway_admin/src/components/UserCard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import blank from '../assets/blank-profile.webp'
+import moment from 'moment'
 
 export default function UserCard({ data }) {
     return (
@@ -7,10 +8,11 @@ export default function UserCard({ data }) {
             <img src={data?.image?.url || blank} className='userImage' />
             <div>
                 <div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
+                    <div>{data.username}</div>
+                    <div>{data.email}</div>
+                    <div>{data.phone}</div>
+                    <div>{data.gender}</div>
+                    <div>{moment(data.dob).format('DD-MM-YYYY')}</div>
                 </div>
                 <div>
 
