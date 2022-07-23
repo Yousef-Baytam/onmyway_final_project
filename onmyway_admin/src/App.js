8bc0047316959c5bda51d94a7acce59e9ba305e3
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-23 17:22:48 +0300

    Front-End-Admin: Added the main 2 routes to be used

--- onmyway_admin/src/App.js
+++ onmyway_admin/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      hello
+      <Routes>
+        <Route
+          path="/"
+          element={<Auth />}
+        ></Route>
+        <Route
+          path="/contacts"
+          element={<Contacts />}
+        ></Route>
+      </Routes>
     </div>
   );
 }
