fb08bb7349f88e9f22f71e0490229f15c5ca74a8
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-23 18:12:32 +0300

    Front-End-Admin: Added the auth props and login function

--- onmyway_admin/src/pages/Auth.js
+++ onmyway_admin/src/pages/Auth.js
@@ -2,16 +2,20 @@ import React from 'react'
 import logo from '../assets/logo.png'
 import Input from '../components/Input'
 
-export default function Auth() {
-    console.log(logo)
+export default function Auth({ username, setUsername, password, setPassword }) {
+
+    const handleLogin = async () => {
+        null
+    }
+
     return (
         <div className='App'>
             <img src={logo} className='logo' />
             <form >
                 <div>
                     <Input type={'text'} name={'username'} placeholder={'Username'} value={username} setValue={setUsername} />
-                    <Input type={'password'} name={'passwordRegister'} placeholder={'Password'} value={password} setValue={setPassword} />
-                    <Submit value={'Login'} run={handleRegister} />
+                    <Input type={'password'} name={'password'} placeholder={'Password'} value={password} setValue={setPassword} />
+                    <Submit value={'Login'} run={handleLogin} />
                 </div>
             </form>
         </div>
