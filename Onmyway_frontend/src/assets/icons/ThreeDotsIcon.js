c372822e6ce1ad6013825bd4e61125cfd7f7e70d
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-23 13:22:00 +0300

    Front-End: Themed the profile screen options

--- Onmyway_frontend/src/assets/icons/ThreeDotsIcon.js
+++ Onmyway_frontend/src/assets/icons/ThreeDotsIcon.js
@@ -1,17 +1,18 @@
 import Svg, { Path } from 'react-native-svg';
+import { useTheme } from '../../context/ThemeContext';
 
 export default function ThreeDotsIcon() {
+    const { theme } = useTheme()
 
     return (
         <Svg
             width={40}
             height={20}
             viewBox="0 0 4 20"
-            fill='#858585'
+            fill={theme.outline}
         >
             <Path
                 d="M2 4a2 2 0 1 0 0-4 2 2 0 0 0 0 4ZM2 12a2 2 0 1 0 0-4 2 2 0 0 0 0 4ZM2 20a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z"
-                fill="#000"
             />
         </Svg>
     );
