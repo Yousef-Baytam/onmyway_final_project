9daaf28b01a778c00ec2147fe38f44dcce9683ad
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-24 16:54:16 +0300

    Front-End-Admin: Finalized the handleReviewed function

--- onmyway_admin/src/components/ReportCard.js
+++ onmyway_admin/src/components/ReportCard.js
@@ -3,12 +3,17 @@ import blank from '../assets/blank-profile.webp'
 import moment from 'moment'
 import BanIcon from '../assets/icons/BanIcon'
 import ActivateIcon from '../assets/icons/ActivateIcon'
+import { setReportsStatus } from '../controllers/reportController'
 
 export default function ReportCard({ data }) {
     console.log(data)
 
     const handleReviewed = async () => {
-
+        try {
+            await setReportsStatus(data._id)
+        } catch (e) {
+            console.log(e)
+        }
     }
 
     return (
