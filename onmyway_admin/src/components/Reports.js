18b0cd9578a5136be4fb3670b6135da22a747d59
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-24 16:05:52 +0300

    Front-End-Admin: Added states for the reports types and a useEffect to get the reports

--- onmyway_admin/src/components/Reports.js
+++ onmyway_admin/src/components/Reports.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../components/Header'
 
 export default function Reports() {
+    const [pendingReports, setPendingReports] = useState([])
+    const [reviewedReports, setReviewedReports] = useState([])
+
+    useEffect(() => {
+        handleGetReports()
+    }, [])
     return (
         <div>
             <Header title={'Reports'} items={['None']} />
