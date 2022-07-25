ae3ecc376ee16b124530852a085ef74bc79078c3
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-25 18:50:34 +0300

    Front-End: Added an unblock button and adjusted the structure of the card and its style

--- onmyway_frontend/src/components/BlockedUserCard.js
+++ onmyway_frontend/src/components/BlockedUserCard.js
@@ -2,18 +2,22 @@ import { View, Text, StyleSheet, Image } from 'react-native'
 import React from 'react'
 import Pressable from 'react-native/Libraries/Components/Pressable/Pressable'
 import { useTheme } from '../context/ThemeContext'
+import CustomButton from './CustomButton'
 
 const BlockedUserCard = ({ data, action }) => {
     const { theme } = useTheme()
 
     return (
         <Pressable style={[styles.container, { backgroundColor: theme.bg }]} onPress={action}>
-            <Image style={styles.image} source={data?.image ? data.image?.url ? { uri: data.image.url } : require('../assets/blank-profile.webp') : require('../assets/blank-profile.webp')} />
-            <View style={styles.userInfo}>
-                <View style={styles.mesasgeInfoWrapper}>
-                    <Text style={{ color: theme.text }}>{data.username}</Text>
+            <View style={[styles.subContainer, { backgroundColor: theme.bg }]}>
+                <Image style={styles.image} source={data?.image ? data.image?.url ? { uri: data.image.url } : require('../assets/blank-profile.webp') : require('../assets/blank-profile.webp')} />
+                <View style={styles.userInfo}>
+                    <View style={styles.mesasgeInfoWrapper}>
+                        <Text style={{ color: theme.text }} numberOfLines={1}>{data.username}</Text>
+                    </View>
                 </View>
             </View>
+            <CustomButton text={'Unblock'} custom={{ width: '30%' }} />
         </Pressable>
     )
 }
@@ -21,7 +25,13 @@ const BlockedUserCard = ({ data, action }) => {
 const styles = StyleSheet.create({
     container: {
         width: '100%',
-        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        flexDirection: 'row',
+        padding: 15
+    },
+    subContainer: {
+        width: '60%',
         alignItems: 'center',
         justifyContent: 'flex-start',
         flexDirection: 'row',
