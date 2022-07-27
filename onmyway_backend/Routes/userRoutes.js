4c68dd7384dab7fbe68ee40b4d8f24bc8d0ddda2
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-07-28 01:58:42 +0300

    Back-End: Changed the order of the patch routes

--- onmyway_backend/Routes/userRoutes.js
+++ onmyway_backend/Routes/userRoutes.js
@@ -5,11 +5,11 @@ const catchAsync = require('../Utils/catchAsync')
 
 router.post('/', catchAsync(userController.getUser))
 router.post('/report/:id', catchAsync(userController.reportUser))
-router.patch('/:id/:token', catchAsync(userController.storeToken))
 router.patch('/notification', catchAsync(userController.updateStatus))
 router.patch('/block/:id', catchAsync(userController.blockUser))
 router.patch('/unblock/:id', catchAsync(userController.unblockUser))
 router.patch('/', catchAsync(userController.updateUser))
 router.patch('/image', catchAsync(userController.updateImage))
+router.patch('/:id/:token', catchAsync(userController.storeToken))
 
 module.exports = router
