6564a4a06aea699522b8c9b8eee19700b609946e
Yousef Baytam <yousefbaytam.yb@gmail.com>
2022-08-02 11:45:13 +0300

    Front-End: Moved all the firebase config details to the .env

--- onmyway_frontend/firebase.js
+++ onmyway_frontend/firebase.js
@@ -1,15 +1,15 @@
 import { initializeApp } from 'firebase/app'
 import { initializeFirestore } from 'firebase/firestore'
-import { firebaseApiKey } from '@env'
+import { firebaseApiKey, authDomain, projectId, storageBucket, messagingSenderId, appId, measurementId } from '@env'
 
 const firebaseConfig = {
     apiKey: firebaseApiKey,
-    authDomain: "onmyway-168af.firebaseapp.com",
-    projectId: "onmyway-168af",
-    storageBucket: "onmyway-168af.appspot.com",
-    messagingSenderId: "976356158167",
-    appId: "1:976356158167:web:fc4bbbc673a58cac1f9e92",
-    measurementId: "G-C1VHM8EREY"
+    authDomain: authDomain,
+    projectId: projectId,
+    storageBucket: storageBucket,
+    messagingSenderId: messagingSenderId,
+    appId: appId,
+    measurementId: measurementId
 }
 
 const app = initializeApp(firebaseConfig)
